032eaba006b9fdd89f30f5d6b8dca90c967b993e
Harsh <hgaur491@gmail.com>
2024-04-16 12:15:58 +0530

    testing select option shadeCN

---
+++ src/app/(home)/anime/filter/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FilterForm from "@/components/pages/filter/FilterForm";
+export default function Filter() {
+    return (
+        <div>
+            <FilterForm />
+        </div>
+    );
+}
