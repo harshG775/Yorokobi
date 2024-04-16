d3c8eb92610c0b83c875af0bab238975473f9bcf
Harsh <hgaur491@gmail.com>
2024-04-16 15:55:58 +0530

    creating action for creating code verifier

---
+++ src/actions/actions.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import generateCodeVerifier from "@/utils/MAL/auth/generateCodeVerifier";
+import { envServer } from "@/utils/env/envServer";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function redirect_MAL_ToVerify() {
+    const { authUrl, codeVerifier } = generateCodeVerifier(envServer.MAL_CLIENT_ID, "http://localhost:4000/verify");
+    cookies().set("codeVerifier", codeVerifier);
+    redirect(authUrl);
+}
+
--- src/app/(auth)/redirect/page.tsx
+++ src/app/(auth)/redirect/page.tsx
@@ -1,9 +1,10 @@
-import generateCodeVerifier from "@/utils/MAL/auth/generateCodeVerifier";
-import { envServer } from "@/utils/env/envServer";
-import { redirect } from "next/navigation";
-
-export default function page() {
-    const { authUrl, codeVerifier } = generateCodeVerifier(envServer.MAL_CLIENT_ID,"http://localhost:4000/verify");
-    console.log(codeVerifier);
-    redirect(authUrl);
+"use client"
+import { redirect_MAL_ToVerify } from "@/actions/actions";
+export default function Page() {
+    const handleClick = () => {
+        redirect_MAL_ToVerify();
+    }
+    return (
+        <div onClick={handleClick}> click</div>
+    )
 }
