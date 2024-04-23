b4ea66fef98609a595eb6a9e5e8f642617feb764
Harsh <hgaur491@gmail.com>
2024-04-23 19:45:42 +0530

    including episode as id for disqus

--- web/src/app/(home)/anime/[id]/page.tsx
+++ web/src/app/(home)/anime/[id]/page.tsx
@@ -8,21 +8,27 @@ import { cn } from "@/lib/utils";
 import { envServer } from "@/utils/env/envServer";
 
 type CommentsProps = {
-    data: { id: string; title: string }; 
-    className: string
+    data: { id: string; title: string };
+    className: string;
+    episode?: string | undefined;
 };
-function Comments({ data, className }: CommentsProps) {
+function Comments({ data, episode, className }: CommentsProps) {
     return (
         <div className={cn("p-4", className)}>
-            <DisqusAnime id={data.id} title={data.title} />
+            <DisqusAnime id={data.id + episode} title={data.title} />
         </div>
     );
 }
 
 type AnimeOverviewProps = {
     params: { id: string };
+    searchParams: { ep?: string | undefined };
 };
-export default async function page({ params }: AnimeOverviewProps) {
+export default async function page({
+    params,
+    searchParams,
+}: AnimeOverviewProps) {
+    console.log(searchParams);
     try {
         const { data } = await Axios.get(
             `anime/${params.id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics,country,time`,
@@ -42,7 +48,11 @@ export default async function page({ params }: AnimeOverviewProps) {
                     <Related data={data} />
                 </div>
                 <div className="grid xl:grid-cols-[3fr,1fr]">
-                    <Comments className="order-2 xl:order-none " data={data} />
+                    <Comments
+                        className="order-2 xl:order-none "
+                        data={data}
+                        episode={searchParams?.ep}
+                    />
                     <Recommended data={data} />
                 </div>
             </main>
