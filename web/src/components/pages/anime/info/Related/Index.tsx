239d54398137bf056adfcdeab9dc9f22059bf86a
Harsh <hgaur491@gmail.com>
2024-04-23 11:39:42 +0530

    ////

--- web/src/components/pages/anime/info/Related/Index.tsx
+++ web/src/components/pages/anime/info/Related/Index.tsx
@@ -1,3 +1,4 @@
+import HorizontalCard from "@/components/ui/cards/HorizontalCard";
 import Image from "next/image";
 import Link from "next/link";
 type PlayerProps = {
@@ -22,25 +23,66 @@ export default function Related({ data }: PlayerProps) {
     const { related_anime } = data;
     console.log(data);
     return (
-        <aside>
-            <h1>Related</h1>
-            <div className="max-h-full overflow-y-auto ">
+        <Section title="Related">
+            <SectionList>
                 {related_anime.map(({ node }) => (
-                    <Link
-                        href={`/anime/${node.id}`}
-                        className={`flex mt-4 text-blue-500`}
-                        key={node.id}
-                    >
-                        <Image
-                            src={node.main_picture.large}
-                            alt={""}
-                            width={100}
-                            height={100}
-                        />
-                        <span className="text-sm">{node.title}</span>
-                    </Link>
+                    <SectionListItem key={node.id}>
+                        <Link
+                            href={`/anime/${node.id}`}
+                            className={`flex mt-4 text-blue-500`}
+                            key={node.id}
+                        >
+                            <Image
+                                src={node.main_picture.large}
+                                alt={""}
+                                width={100}
+                                height={100}
+                            />
+                            <span className="text-sm">{node.title}</span>
+                        </Link>
+                    </SectionListItem>
                 ))}
-            </div>
-        </aside>
+            </SectionList>
+        </Section>
     );
+}
+
+
+
+
+type SectionProps = {
+    title: string;
+    children: React.ReactNode;
+};
+
+function Section({ title, children }: SectionProps) {
+    return (
+        <section className="bg-neutral-200">
+            <div>
+                <h2 className="text-xl font-semibold text-rose-500">{title}</h2>
+            </div>
+            {children}
+        </section>
+    );
+}
+type SectionListProps = {
+    children: React.ReactNode;
+};
+function SectionList({children}:SectionListProps) {
+
+    return (
+        <ul className="max-h-full overflow-y-auto ">
+            {children}
+        </ul>
+    )
+}
+type SectionListItemProps = {
+    children: React.ReactNode;
+};
+function SectionListItem({children}:SectionListItemProps) {
+    return (
+        <li>
+            {children}
+        </li>
+    )
 }
