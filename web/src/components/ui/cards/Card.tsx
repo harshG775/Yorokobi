b648403a6c4620c2a7376955b4591d8feb87c944
Harsh <hgaur491@gmail.com>
2024-05-23 16:36:41 +0530

    fixing card image overflow size

--- web/src/components/ui/cards/Card.tsx
+++ web/src/components/ui/cards/Card.tsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 
 export default function Card({ data }: any) {
     return (
-        <li className="grid grid-rows-[1fr_auto] hover:scale-105 transition-transform ease-out duration-200 ">
-            <Link href={`/watch/${data.id}`} className="h-56 overflow-hidden rounded-t-md">
+        <li className="group overflow-hidden rounded-md grid grid-rows-[3fr_1fr] hover:scale-105 transition-transform ease-out duration-200">
+            <Link href={`/watch/${data.id}`}>
                 <Image
                     src={data.image}
                     alt={data.title}
                     width={170}
                     height={224}
-                    className="w-full object-cover rounded-md"
+                    className="h-full w-full"
                 />
             </Link>
-            <div className=" p-2 shadow-md rounded-md">
-                <h3 className="font-medium text-sm line-clamp-2 hover:text-primary ">{data.title}</h3>
+            <Link href={`/watch/${data.id}`} className="px-2 shadow-md rounded-md grid grid-rows-2">
+                <h3 className="font-medium text-sm  group-hover:text-primary ">
+                    <span className="line-clamp-2">{data.title}</span>
+                </h3>
                 <div className="text-xs">
                     <span>TV</span> - <span>23 min</span>
                 </div>
-            </div>
+            </Link>
         </li>
     );
 }
