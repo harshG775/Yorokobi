b885eb9c1172082d4e1e0da1c3cfa9117ad1bf3f
Harsh <hgaur491@gmail.com>
2024-06-14 20:17:53 +0530

    ///

--- web/src/app/(pages)/anime/[id]/page.tsx
+++ web/src/app/(pages)/anime/[id]/page.tsx
@@ -14,18 +14,21 @@ export default async function AnimePage({
     const data = await getInfoById(id);
     console.log(data);
     return (
-        <main className="space-y-10 py-2">
-            <div className="container py-2 
+        <main className="space-y-2  py-2  [&_section]:border [&_section]:border-primary">
+            <div className="container
                 grid gap-2 lg:grid-cols-[1fr_300px]
                 ">
                 <div className="lg:order-1 grid gap-2 2xl:grid-cols-[200px_1fr]">
-                    <section className="order-2 2xl:order-1">Episode</section>
                     <section className="order-1 2xl:order-2 aspect-video">Player</section>
+                    <section className="order-2 2xl:order-1 min-h-40">Episode</section>
                 </div>
-                <section className="lg:order-3">Overview</section>
-                <section className="lg:order-2 lg:row-span-2">Related</section>
+                <section className="lg:order-3">
+                    <div className="min-h-96">Overview</div>
+                    <div className="min-h-40">seasons if there is any</div>
+                </section>
+                <section className="lg:order-2 lg:row-span-2 min-h-96">Related</section>
             </div>
-            <div className="container grid gap-y-10 gap-x-2 lg:grid-cols-[1fr_300px]">
+            <div className="container grid gap-2 lg:grid-cols-[1fr_300px]">
                 <section className="min-h-96">Comments</section>
                 <section className="min-h-96">Recommended</section>
             </div>
